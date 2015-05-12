03fc9d325ecb6e7ea16c2e984f609175e0f53f72
Ryan Wey <wey.ryan@gmail.com>
2015-05-12 08:49:26 -0700

    Health now Interface
    ^^

--- src/Model/Fireflower.java
+++ src/Model/Fireflower.java
@@ -6,4 +6,10 @@ public class Fireflower extends Entity implements Powerup{
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public void act() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
--- src/Model/Health.java
+++ src/Model/Health.java
@@ -1,9 +1,5 @@
 package Model;
 
-public abstract class Health {
-
-	public Health() {
-		// TODO Auto-generated constructor stub
-	}
+public interface Health {
 
 }
