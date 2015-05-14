c78f185af7b830e8852e49b36f0510a0332dd59f
Ryan Wey <wey.ryan@gmail.com>
2015-05-14 08:06:52 -0700

    Entity Revision
    #swag

--- src/Model/Entity.java
+++ src/Model/Entity.java
@@ -1,9 +1,44 @@
 package Model;
 
-public abstract class Entity {
+import java.awt.Image;
 
-	public Entity() {
+public class Entity {
+	
+	int xLoc, yLoc;
+	boolean status, hasHealth;
+	
+	Image sprite;
+
+	public Entity(int x, int y, Image icon) {
 		// TODO Auto-generated constructor stub
+		xLoc = x;
+		yLoc = y;
+		sprite = icon;
+	}
+	
+	public Image getSprite()
+	{
+		return sprite;
+	}
+	
+	public int getX()
+	{
+		return xLoc;
+	}
+	
+	public int getY()
+	{
+		return yLoc;
+	}
+	
+	public void act()
+	{
+	
+	}
+	
+	public boolean getStatus()
+	{
+		return status;
 	}
 
 }
