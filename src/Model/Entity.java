c9878d13d8d712ddad246a3ed266f4b023cdf762
Ryan Wey <wey.ryan@gmail.com>
2015-05-14 08:16:59 -0700

    Solved Entity Merge COnflict
    Conflicts:
    	src/Model/Entity.java

--- src/Model/Entity.java
+++ src/Model/Entity.java
@@ -1,13 +1,12 @@
 package Model;
 
-<<<<<<< HEAD
 import java.awt.Image;
 
 public class Entity {
-	
+
 	int xLoc, yLoc;
 	boolean status, hasHealth;
-	
+
 	Image sprite;
 
 	public Entity(int x, int y, Image icon) {
@@ -16,57 +15,29 @@ public class Entity {
 		yLoc = y;
 		sprite = icon;
 	}
-	
+
 	public Image getSprite()
 	{
 		return sprite;
 	}
-	
+
 	public int getX()
 	{
 		return xLoc;
 	}
-	
+
 	public int getY()
 	{
 		return yLoc;
 	}
-	
+
 	public void act()
 	{
-	
+
 	}
-	
+
 	public boolean getStatus()
 	{
 		return status;
-=======
-import java.awt.Color;
-import java.awt.Image;
-
-public abstract class Entity {
-	int x, y;
-	Image img;
-	Color color;
-
-	public Entity(int xC, int yC, Image icon) {
-		x = xC;
-		y = yC;
-		img = icon;
-	}
-	
-	public Image getImg(){
-		return img;
-	}
-	
-	public abstract void act();
-	
-	public int getX(){
-		return x;
-	}
-	
-	public int getY(){
-		return y;
->>>>>>> origin/master
 	}
 }
