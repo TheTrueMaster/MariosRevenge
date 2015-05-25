6ab9a995c7fcfdeabad1dbe2a7a541d4b0b1dbbf
Ronak <ronakshahultimate@gmail.com>
2015-05-24 21:00:37 -0700

    Final Fix

--- src/Controller/Render.java
+++ src/Controller/Render.java
@@ -23,27 +23,9 @@ public class Render {
 	}
 
 	public ImageIcon getImage(char c) {
-<<<<<<< HEAD
+
 		return pics.get(c);
 
-=======
-		// TODO Auto-generated method stub
-		//testing class loader
-		ClassLoader cldr = this.getClass().getClassLoader();
-
-		ImageIcon img = new ImageIcon("res/fish.gif");
-
-		URL u = cldr.getResource("fish.gif");
-		
-
-		ImageIcon returnMe = new ImageIcon(u);
-		
-		return returnMe; //not sure if this works. :/
-
-		//stubCode
-		//return pics.get(c);
-		//^^ what the code will be eventually
->>>>>>> 6fb1a4e3d71514f6538fcfe6cfc1c4b1475b895d
 	}
 
 
