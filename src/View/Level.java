e7c7924a2191ca681414a494e1413a942cff26d3
Ronak <ronakshahultimate@gmail.com>
2015-05-30 21:42:15 -0700

    Revert "PLATFORMS"
    This reverts commit 4c94da514eda8fefc8f42dde10935603f0d2cb34.

--- src/View/Level.java
+++ src/View/Level.java
@@ -313,13 +313,12 @@ public class Level extends JPanel implements KeyListener, ActionListener{
 				}
 				if(ent == null){
 					//Similar code to 'case: 90' 
-					p.moveUp(p.getVelY());
-					int temp = (int)(p.getY() / 24);
+					int temp = (int)(Math.abs(p.moveUp(p.getVelY())) / 24);
 					if (temp > 0)
 					{
 						level[p.getRow()][p.getCol()] = ' ';
-						p.setRow(temp + 2);
-						level[temp + 2][p.getCol()] = 'P';
+						p.setRow(p.getRow() + temp);
+						level[p.getRow() + temp][p.getCol()] = 'P';
 					}
 				}
 
