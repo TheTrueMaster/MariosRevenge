0d36de901d0e7487e58790cac7cf7c6627c81288
Ryan Wey <wey.ryan@gmail.com>
2015-05-31 14:25:54 -0700

    :P

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -231,6 +231,12 @@ public class Player extends Entity {
 		//y velocity and gravity to move player.
 		int temp = yLoc;
 		yLoc += velY + Level.gravity;
+		
+		if (hitPlatform)
+		{
+			hitPlatform = false;
+			setVelY(0);
+		}
 
 		//These changes should only occur if the player is falling.
 		//If the player is on a platform, it is no longer falling and 
@@ -239,11 +245,7 @@ public class Player extends Entity {
 		if (falling)
 		{
 
-			if (hitPlatform)
-			{
-				hitPlatform = false;
-				setVelY(0);
-			}
+
 			// if 
 			//velY + gravity
 
@@ -252,49 +254,48 @@ public class Player extends Entity {
 			//Terminal velocity is gravity (5). So this checks if the difference
 			//between current velocity and terminal vel is at more than or equal 
 			//to gravity. If so, it decreases the velocity.
-			else{
-				if (getVelY() < 0)
+			if (getVelY() < 0)
+			{
+
+				if (getVelY() > -5)
 				{
+					setVelY(0);
+				}
+				//if (Level.gravity - getVelY() >= 5){
+				else{
+					setVelY(getVelY() + Level.gravity);
+				}
+				//}
 
-					if (getVelY() > -5)
-					{
-						setVelY(0);
-					}
-					//if (Level.gravity - getVelY() >= 5){
-					else{
-						setVelY(getVelY() + Level.gravity);
-					}
-					//}
-
-					//otherwise if the velocity is not terminal but the difference
-					//is less than the magnitude of gravity than it decreases 
-					//y velocity by whatever's left.
-					/*	else if (velY < 5){
+				//otherwise if the velocity is not terminal but the difference
+				//is less than the magnitude of gravity than it decreases 
+				//y velocity by whatever's left.
+				/*	else if (velY < 5){
 
 					setVelY(Level.gravity);
 				} */
+			}
+
+			else if (getVelY() > 0)
+			{
+				if (Level.gravity - getVelY() <= 5){
+					setVelY(getVelY() + Level.gravity);
 				}
 
-				else if (getVelY() > 0)
-				{
-					if (Level.gravity - getVelY() <= 5){
-						setVelY(getVelY() + Level.gravity);
-					}
+				//otherwise if the velocity is not terminal but the difference
+				//is less than the magnitude of gravity than it decreases 
+				//y velocity by whatever's left.
+				else if (velY < 5){
 
-					//otherwise if the velocity is not terminal but the difference
-					//is less than the magnitude of gravity than it decreases 
-					//y velocity by whatever's left.
-					else if (velY < 5){
-
-						setVelY(Level.gravity);
-					}
+					setVelY(Level.gravity);
 				}
 			}
 		}
-
-
-
 	}
+
+
+
+
 
 
 	/**
